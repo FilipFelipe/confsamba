2f1b8f8ef984055b14e8cc5d058fc74fd3970bcf
Filip Felipe <filip_junio@hotmail.com>
2019-11-16 21:13:42 -0300

    update install

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+echo "iniciando"
+yum install figlet
+figlet Samba - IFSP
+echo -e "Clique em \e[1;33Qualquer\e[0m tecla para continuar..."
+read -n 1
 echo "instalando samba"
 #instalando samba
 yum install samba samba-client samba-doc samba-swat
@@ -12,23 +17,26 @@ mkdir /home/arquivos
 mkdir /home/admin
 
 
-
+sleep 2
 clear
 echo "fazendo backup"
 hoje=$(date +%F)
 #criando backup com a data atual
-cp /etc/samba/smb.conf /etc/samba/smb.conf_backup$hoje
+cp /etc/samba/smb.conf /etc/samba/smb.conf_backup_$hoje
 rm /etc/samba/smb.conf
 cp smb.conf /etc/samba
+sleep 2
 clear 
 
 echo "configurando usuarios e grupos ..."
+sleep 2
 #criando grupos 
 echo "Criando grupos"
 groupadd financeiro 
 groupadd admin 
 
-
+sleep 2
+echo "Criando usuarios"
 #criando usuarios de controle 
 useradd admin_controle
 useradd financeiro_controle
@@ -46,8 +54,20 @@ chmod 777 -R /home/publica
 chmod 770 -R /home/financeiro
 chmod 755 -R /home/arquivos
 chmod 770 -R /home/admin
-
-echo "finalizado"
+clear 
+echo "atribuindo senha admin_controle"
 smbpasswd -a admin_controle
+echo "atribuindo senha financeiro_controle"
 smbpasswd -a financeiro_controle
+clear
+echo "configuração do samba finalizada !!!"
+read op
+echo "Deseja reiniciar o samba ?"
 
+sleep 2
+echo "reinciando o samba"
+systemctl stop smb
+systemctl start smb 
+sleep 1
+echo "finalizado"
+systemctl status smb
